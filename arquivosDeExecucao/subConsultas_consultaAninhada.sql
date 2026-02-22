cf54046036ea6e1de604f9db81e171116a23b607
Henriquenxb <henriquenxb@hotmail.com>
2026-02-22 00:55:22 -0300

    Aprendendo consulta com GROUP BY -  HAVING e a diferenca para o uso do WHERE

--- arquivosDeExecucao/subConsultas_consultaAninhada.sql
+++ arquivosDeExecucao/subConsultas_consultaAninhada.sql
@@ -8,3 +8,40 @@ where ID = (
 );
 --///////////FIM CONSULTA///////////
 
+--///////////INÍCIO CONSULTA///////////
+--Consulta para filtrar ocorrencias de pedidos com mês '01'
+SELECT * FROM pedidos
+where strftime('%m',datahorapedido) = '01';
+--///////////FIM CONSULTA///////////
+
+
+--///////////INÍCIO CONSULTA///////////
+--Consultas aninhadas. Note que uso o operador 'IN' para fazer referência a TODOS os retornos da consulta interna.
+SELECT nome
+FROM clientes
+WHERE id IN (
+    SELECT idcliente
+    from pedidos
+    where strftime('%m', datahorapedido) = '01'
+);
+--///////////FIM CONSULTA///////////
+
+--///////////INÍCIO CONSULTA///////////
+--Saber a média dos valores dos produtos
+SELECT AVG(preco) From produtos;
+
+SELECT nome, preco
+FROM produtos
+where preco > (
+    SELECT AVG(preco)
+    FROM produtos
+);
+
+SELECT nome, preco
+FROM produtos
+GROUP BY nome, preco
+HAVING preco > (
+    SELECT AVG(preco)
+    FROM produtos
+);
+--///////////FIM CONSULTA///////////
